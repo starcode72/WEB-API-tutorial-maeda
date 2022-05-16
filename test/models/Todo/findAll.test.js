b8333874188f4a481b059b748133dc6507d65ea5
starcode72 <93559665+starcode72@users.noreply.github.com>
2022-05-16 16:44:40 +0900

    created test

--- test/models/Todo/findAll.test.js
+++ test/models/Todo/findAll.test.js
@@ -4,6 +4,26 @@ const Todo = require("../../../models/Todo");
 describe("Todo.findAll MethodsTest", () => {
   it("hoge", () => {
     const todos = Todo.findAll();
+    assert.equal(Array.isArray(todos), true);
+    assert.equal(todos.length > 0, true);
+    todos.forEach((todo) => {
+      //参照値でのテスト
+      assert.deepEqual(todo, {
+        id: todo.id,
+        title: todo.title,
+        body: todo.body,
+        createdAt: todo.createdAt,
+        updatedAt: todo.updatedAt,
+      });
+      // スプレッド演算子バージョン (より厳格なテスト)
+      // assert.deepStrictEqual({ ...todo}, {
+      //   id: todo.id,
+      //   title: todo.title,
+      //   body: todo.body,
+      //   createdAt: todo.createdAt,
+      //   updatedAt: todo.updatedAt,
+      // });
+    });
 
     console.log(todos, "@@@@@@@@@@");
   });
