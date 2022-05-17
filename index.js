66cc1cd7854e31f24b87e0c8bee913818bf6208c
starcode72 <93559665+starcode72@users.noreply.github.com>
2022-05-17 18:23:13 +0900

    supertest

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-const express = require("express");
-const app = express();
+// const express = require("express");
+// const app = express();
+// const todosRouter = require("./routers/todos");
+// 外部に切り出した app.jsをrequireインポート
+const app = require('./app')
 const PORT = 8080;
-const todosRouter = require("./routers/todos");
 
-app.use("/api/todos", todosRouter);
+// app.use("/api/todos", todosRouter);
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
+
+// index.js元の形
+// const express = require("express");
+// const app = express();
+// const todosRouter = require("./routers/todos");
+// const PORT = 8080;
+
+// app.use("/api/todos", todosRouter);
+
+// app.listen(PORT, () => {
+//   console.log(`Example app listening on port ${PORT}`);
+// });
